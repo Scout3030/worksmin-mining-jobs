ea5065b05f35de63cf2c1eaa10f698ccf0f7ff86
Scout <roberth.r.j.30@gmail.com>
2020-01-21 18:35:35 -0500

    Guardando cambios para fusionar paginas

--- public/js/app.js
+++ public/js/app.js
@@ -49592,8 +49592,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\agenda-minera\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\agenda-minera\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\mineria\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\mineria\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
